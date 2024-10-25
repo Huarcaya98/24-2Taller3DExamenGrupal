f56cc53387c830fb44eba1aa76c02b36ef3dd3af
JehuRodriguez <j.eh.u14@hotmail.com>
2024-10-25 09:51:00 -0500

    Agregando al TUMI

---
+++ Assets/MovimientoJugador.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoJugador : MonoBehaviour
+{
+
+    public class JugadorMovimiento : MonoBehaviour
+    {
+        public float moveSpeed = 5f;
+        private Rigidbody rb;
+        private VictoryScreen victoryScreenManager;
+
+        public PalancaJesus palanca;
+        public AntorchaJesus antorcha;
+
+
+        void Start()
+        {
+
+            rb = GetComponent<Rigidbody>();
+            victoryScreenManager = FindObjectOfType<VictoryScreen>();
+        }
+
+
+        void FixedUpdate()
+        {
+
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+
+
+            Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+
+            rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+
+            if (other.CompareTag("TUMI"))
+            {
+                victoryScreenManager.CollectPiece();
+                Destroy(other.gameObject);
+            }
+
+            if (other.CompareTag("Palanca"))
+            {
+
+                palanca.Interactuar();
+            }
+
+            if (other.CompareTag("Antorcha"))
+            {
+                antorcha.Interactuar();
+            }
+
+        }
+
+
+    }
+
+}
